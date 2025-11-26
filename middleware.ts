ff16d20f1ef4ea3ad12ac79a24d8fe101aa2d7de
Chaitanya Kumar R <chaitu.raju@gmail.com>
2025-11-26 19:36:01 +0530

    Removed db operations from middleware to check whether it is causing errors in production

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,26 @@
 import { NextResponse } from "next/server";
 import { auth } from "./auth";
-import { prisma } from "./lib/prisma";
+// import { prisma } from "./lib/prisma";
 import { InputJsonValue } from "./generated/prisma/runtime/library";
 import { Prisma } from "./generated/prisma";
 export const runtime = "nodejs";
 export default auth(async (request) => {
-  if (Math.random() < 0.1) {
-    const thresholdTime = new Date(Date.now() - 60 * 1000);
-    await prisma.cart.deleteMany({
-      where: {
-        userId: null,
-        updatedAt: {
-          lt: thresholdTime,
-        },
-      },
-    });
-  }
+  // if (Math.random() < 0.1) {
+  //   const thresholdTime = new Date(Date.now() - 60 * 1000);
+  //   await prisma.cart.deleteMany({
+  //     where: {
+  //       userId: null,
+  //       updatedAt: {
+  //         lt: thresholdTime,
+  //       },
+  //     },
+  //   });
+  // }
   const cartSessionIdCookie = request.cookies.get("cartSessionId");
+  const response = NextResponse.next();
   if (!cartSessionIdCookie) {
     const cartSessionId = crypto.randomUUID();
-    console.log(cartSessionId);
-    const response = NextResponse.next();
+    // console.log(cartSessionId);
     response.cookies.set("cartSessionId", cartSessionId, {
       httpOnly: true,
       secure: process.env.NODE_ENV == "production",
@@ -36,25 +36,25 @@ export default auth(async (request) => {
       },
     });
     if (cart && cart.items)
-      await prisma.cart.update({
-        where: {
-          id: cart.id,
-        },
-        data: {
-          items: cart.items as Prisma.CartUpdateitemsInput[],
-        },
-      });
-    const response = NextResponse.next();
-    response.cookies.set(
-      "cartSessionId",
-      cartSessionIdCookie?.value as string,
-      {
-        httpOnly: true,
-        secure: process.env.NODE_ENV == "production",
-        sameSite: "lax",
-        maxAge: 60,
-      }
-    );
+      // await prisma.cart.update({
+      //   where: {
+      //     id: cart.id,
+      //   },
+      //   data: {
+      //     items: cart.items as Prisma.CartUpdateitemsInput[],
+      //   },
+      // });
+      // const response = NextResponse.next();
+      response.cookies.set(
+        "cartSessionId",
+        cartSessionIdCookie?.value as string,
+        {
+          httpOnly: true,
+          secure: process.env.NODE_ENV == "production",
+          sameSite: "lax",
+          maxAge: 60,
+        }
+      );
     return response;
   }
 });
