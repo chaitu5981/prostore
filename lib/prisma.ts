93120a4c4669f153df0ba46731803f587bf7e8e7
Chaitanya Kumar R <chaitu.raju@gmail.com>
2025-11-26 13:16:12 +0530

    modified prisma.ts to work in production

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,22 @@
 import { PrismaClient } from "@/generated/prisma";
-// import { neon } from "@neondatabase/serverless";
-// import { PrismaNeonHTTP } from "@prisma/adapter-neon";
+import { neon, type HTTPQueryOptions } from "@neondatabase/serverless";
+import { PrismaNeonHTTP } from "@prisma/adapter-neon";
 
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const sql = neon(process.env.DATABASE_URL!);
+const adapter = new PrismaNeonHTTP(
+  sql.toString(),
+  {} satisfies HTTPQueryOptions<boolean, boolean>
+);
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({ adapter });
+} else {
+  prisma = globalForPrisma.prisma || new PrismaClient();
+}
 
 if (process.env.NODE_ENV == "development") globalForPrisma.prisma = prisma;
+export { prisma };
