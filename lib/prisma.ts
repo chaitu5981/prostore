ce92750270e09db1dbbb91e52f5a6cecfcae2997
Chaitanya Kumar R <chaitu.raju@gmail.com>
2025-12-08 13:35:31 +0530

    changed to prisma managed postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { neon, type HTTPQueryOptions } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaPg } from "@prisma/adapter-pg";
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
@@ -9,14 +7,11 @@ const connString = process.env.DATABASE_URL;
 if (!connString) {
   throw new Error("DATABASE_URL is not set");
 }
-const adapter =
-  process.env.NODE_ENV === "production"
-    ? new PrismaNeon({ connectionString: connString })
-    : new PrismaPg({ connectionString: connString });
+const adapter = new PrismaPg({ connectionString: connString });
 let prisma: PrismaClient;
 if (process.env.NODE_ENV == "development")
   prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 else prisma = new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV == "development") globalForPrisma.prisma = prisma;
-export { prisma };
+export default prisma;
