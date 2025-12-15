2475770d6b9769c7250d29c685df47f288abcf7e
Chaitanya Kumar R <chaitu.raju@gmail.com>
2025-12-15 16:19:59 +0530

    Added loaders while changing Pagination parameters

--- components/orders/pagination.tsx
+++ components/orders/pagination.tsx
@@ -10,6 +10,8 @@ import {
 } from "../ui/select";
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
+import { useTransition } from "react";
+import Loader from "../Loader";
 
 const Pagination = ({ noOfPages }: { noOfPages: number }) => {
   const limits = [3, 5, 10, 20];
@@ -18,18 +20,30 @@ const Pagination = ({ noOfPages }: { noOfPages: number }) => {
   const params = useSearchParams();
   const page = params.get("page") || "1";
   const limit = params.get("limit") || "3";
+  const [goingToNextPage, startGoingToNextPage] = useTransition();
+  const [goingToPreviousPage, startGoingToPreviousPage] = useTransition();
+  const [changingLimit, startChangingLimit] = useTransition();
+  const goToNextPage = () =>
+    startGoingToNextPage(() => {
+      router.push(`${pathname}?page=${Number(page) + 1}&limit=${limit}`);
+    });
+  const goToPreviousPage = () =>
+    startGoingToPreviousPage(() => {
+      router.push(`${pathname}?page=${Number(page) - 1}&limit=${limit}`);
+    });
+  const changeLimit = (v: string) =>
+    startChangingLimit(() => {
+      router.push(`${pathname}?page=1&limit=${v}`);
+    });
   return (
     <div className="flex-between">
       <div>
         <Label htmlFor="limit" className="my-2">
           Limit
         </Label>
-        <Select
-          value={limit as string}
-          onValueChange={(v) => router.push(`${pathname}?page=1&limit=${v}`)}
-        >
+        <Select value={limit as string} onValueChange={(v) => changeLimit(v)}>
           <SelectTrigger id="limit">
-            <SelectValue />
+            {changingLimit ? <Loader size={20} /> : <SelectValue />}
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -46,20 +60,16 @@ const Pagination = ({ noOfPages }: { noOfPages: number }) => {
         <Button
           disabled={page == "1"}
           variant="outline"
-          onClick={() =>
-            router.push(`${pathname}?page=${Number(page) - 1}&limit=${limit}`)
-          }
+          onClick={goToPreviousPage}
         >
-          Previous
+          {goingToPreviousPage ? <Loader size={20} /> : "Previous"}
         </Button>
         <Button
           disabled={Number(page) == noOfPages}
           variant="outline"
-          onClick={() =>
-            router.push(`${pathname}?page=${Number(page) + 1}&limit=${limit}`)
-          }
+          onClick={goToNextPage}
         >
-          Next
+          {goingToNextPage ? <Loader size={20} /> : "Next"}
         </Button>
       </div>
     </div>
