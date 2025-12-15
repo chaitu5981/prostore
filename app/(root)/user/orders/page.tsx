cde21fb5748c09d7a22885096fde33ef379c9cfd
Chaitanya Kumar R <chaitu.raju@gmail.com>
2025-12-15 15:51:39 +0530

    some correction

--- app/(root)/user/orders/page.tsx
+++ app/(root)/user/orders/page.tsx
@@ -14,12 +14,12 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 const OrdersContent = async ({
-  page,
-  limit,
+  searchParams,
 }: {
-  page: string;
-  limit: string;
+  searchParams: Promise<{ page: string; limit: string }>;
 }) => {
+  const { page, limit } = await searchParams;
+
   const res = await getMyOrders({
     page: Number(page) || 1,
     limit: Number(limit) || 3,
@@ -28,58 +28,65 @@ const OrdersContent = async ({
   return (
     <div className="wrapper">
       <p className="text-xl font-semibold mb-3">Orders</p>
-      <Table className="overflow-x-auto ">
-        <TableHeader>
-          <TableRow>
-            <TableHead>ID</TableHead>
-            <TableHead>DATE</TableHead>
-            <TableHead>TOTAL</TableHead>
-            <TableHead>PAID</TableHead>
-            <TableHead>DELIVERED</TableHead>
-            <TableHead>ACTIONS</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {res.data?.map((order) => (
-            <TableRow key={order.id}>
-              <TableCell>{shortenId(order.id)}</TableCell>
-              <TableCell>{formatDateAndTime(order.createdAt)}</TableCell>
-              <TableCell>
-                {currencyFormatter(order.totalPrice.toString())}
-              </TableCell>
-              <TableCell>
-                {order.isPaid && order.paidAt
-                  ? formatDateAndTime(order.paidAt)
-                  : "Not Paid"}
-              </TableCell>
-              <TableCell>
-                {order.isDelivered && order.deliveredAt
-                  ? formatDateAndTime(order.deliveredAt)
-                  : "Not Delivered"}
-              </TableCell>
-              <TableCell>
-                <Link
-                  href={`/order/${order.id}`}
-                  className="underline text-blue-400"
-                >
-                  Details
-                </Link>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Pagination noOfPages={res.noOfPages as number} />
+      {!res.data || res.data.length == 0 ? (
+        <p className="text-center font-semibold">No Orders</p>
+      ) : (
+        <div>
+          <Table className="overflow-x-auto ">
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>DATE</TableHead>
+                <TableHead>TOTAL</TableHead>
+                <TableHead>PAID</TableHead>
+                <TableHead>DELIVERED</TableHead>
+                <TableHead>ACTIONS</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {res.data?.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>{shortenId(order.id)}</TableCell>
+                  <TableCell>{formatDateAndTime(order.createdAt)}</TableCell>
+                  <TableCell>
+                    {currencyFormatter(order.totalPrice.toString())}
+                  </TableCell>
+                  <TableCell>
+                    {order.isPaid && order.paidAt
+                      ? formatDateAndTime(order.paidAt)
+                      : "Not Paid"}
+                  </TableCell>
+                  <TableCell>
+                    {order.isDelivered && order.deliveredAt
+                      ? formatDateAndTime(order.deliveredAt)
+                      : "Not Delivered"}
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      href={`/order/${order.id}`}
+                      className="underline text-blue-400"
+                    >
+                      Details
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <Pagination noOfPages={res.noOfPages as number} />
+        </div>
+      )}
     </div>
   );
 };
-const OrdersPage = async (props: {
+const OrdersPage = ({
+  searchParams,
+}: {
   searchParams: Promise<{ page: string; limit: string }>;
 }) => {
-  const { page, limit } = await props.searchParams;
   return (
     <Suspense fallback={<Loader size={50} />}>
-      <OrdersContent page={page} limit={limit} />
+      <OrdersContent searchParams={searchParams} />
     </Suspense>
   );
 };
